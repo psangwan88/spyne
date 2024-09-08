b54615b339e5db4586c79cfa9c920f01770ab527
psangwan88 <166609033+psangwan88@users.noreply.github.com>
2024-09-08 23:35:18 +0530

    pom update and unit test

---
+++ Spyne_Selenium/src/test/java/org/spyne/UnitTest.java
@@ -0,0 +1,16 @@
+package org.spyne;
+
+
+import junit.framework.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class UnitTest{
+
+    @Test
+    public void unitTest(){
+        Assert.assertEquals(" Unit test to test syntax for project", 5,5);
+    }
+}
